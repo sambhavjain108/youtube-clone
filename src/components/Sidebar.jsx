067cdd434e2e73a474ba943fdcb8fc1058ea4fb8
Ubuntu <ubuntu@ip-172-31-32-24.ap-south-1.compute.internal>
2026-05-21 11:07:29 +0000

    test

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,36 +1,38 @@
-import React from "react";
-import { Stack } from "@mui/material";
-
-import { categories } from "../utils/constants";
-
-const Categories = ({ selectedCategory, setSelectedCategory }) => (
-  <Stack
-    direction="row"
-    sx={{
-      overflowY: "auto",
-      height: { sx: "auto", md: "95%" },
-      flexDirection: { md: "column" },
+<button
+  className="category-btn"
+  onClick={() => setSelectedCategory(category.name)}
+  style={{
+    background: category.name === selectedCategory ? "#FC1503" : "transparent",
+    color: "white",
+    border: "none",
+    borderRadius: "20px",
+    padding: "10px 15px",
+    margin: "5px 0",
+    cursor: "pointer",
+    transition: "0.3s",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "start",
+    width: "100%",
+  }}
+  key={category.name}
+>
+  <span
+    style={{
+      color: category.name === selectedCategory ? "white" : "red",
+      marginRight: "15px",
+      fontSize: "20px",
     }}
   >
-    {categories.map((category) => (
-      <button
-        className="category-btn"
-        onClick={() => setSelectedCategory(category.name)}
-        style={{
-          background: category.name === selectedCategory && "#FC1503",
-          color: "white",
-        }}
-        key={category.name}
-      >
-        <span style={{ color: category.name === selectedCategory ? "white" : "red", marginRight: "15px" }}>
-          {category.icon}
-        </span>
-        <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}>
-          {category.name}
-        </span>
-      </button>
-    ))}
-  </Stack>
-);
+    {category.icon}
+  </span>
 
-export default Categories;
+  <span
+    style={{
+      opacity: category.name === selectedCategory ? "1" : "0.8",
+      fontWeight: category.name === selectedCategory ? "bold" : "normal",
+    }}
+  >
+    {category.name}
+  </span>
+</button>
