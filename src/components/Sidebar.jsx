59e1bdfd885278587e03fe2a2caa797c71a3972d
sambhavjain108 <152012154+sambhavjain108@users.noreply.github.com>
2026-05-21 16:53:45 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,38 +1,48 @@
-<button
-  className="category-btn"
-  onClick={() => setSelectedCategory(category.name)}
-  style={{
-    background: category.name === selectedCategory ? "#FC1503" : "transparent",
-    color: "white",
-    border: "none",
-    borderRadius: "20px",
-    padding: "10px 15px",
-    margin: "5px 0",
-    cursor: "pointer",
-    transition: "0.3s",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "start",
-    width: "100%",
-  }}
-  key={category.name}
->
-  <span
-    style={{
-      color: category.name === selectedCategory ? "white" : "red",
-      marginRight: "15px",
-      fontSize: "20px",
-    }}
-  >
-    {category.icon}
-  </span>
+import React from "react";
+import { Stack } from "@mui/material";
 
-  <span
-    style={{
-      opacity: category.name === selectedCategory ? "1" : "0.8",
-      fontWeight: category.name === selectedCategory ? "bold" : "normal",
+import { categories } from "../utils/constants";
+
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => (
+  <Stack
+    direction="row"
+    sx={{
+      overflowY: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "column" },
     }}
   >
-    {category.name}
-  </span>
-</button>
+    {categories.map((category) => (
+      <button
+        className="category-btn"
+        onClick={() => setSelectedCategory(category.name)}
+        style={{
+          background: category.name === selectedCategory && "#FC1503",
+          color: "white",
+        }}
+        key={category.name}
+      >
+        <span
+          style={{
+            color:
+              category.name === selectedCategory ? "white" : "red",
+            marginRight: "15px",
+          }}
+        >
+          {category.icon}
+        </span>
+
+        <span
+          style={{
+            opacity:
+              category.name === selectedCategory ? "1" : "0.8",
+          }}
+        >
+          {category.name}
+        </span>
+      </button>
+    ))}
+  </Stack>
+);
+
+export default Sidebar;
